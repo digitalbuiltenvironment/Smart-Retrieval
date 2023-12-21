1ee8e6ffe9a04d547e7609e3422b77f4014589be
Yee Kit <yeek3063@gmail.com>
2023-12-21 09:39:35 +0800

    Update .env example & next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["llamaindex"],
   },
-  env: {
-    CHAT_API: "http://localhost:8000/api/chat",
-  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
