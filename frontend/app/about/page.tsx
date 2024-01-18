ea7fe20bbdd75bf5177292b35c77709be4ea0854
Yee Kit <yeek3063@gmail.com>
2024-01-18 13:25:46 +0800

    Minor UI Updates

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
   return (
     <Main>
       <Header />
-      <div className="rounded-xl shadow-xl p-4 mb-8 z-10 max-w-5xl w-full">
+      <div className="rounded-xl shadow-xl p-4 mb-8 max-w-5xl w-full">
         <div className="max-w-2xl mx-auto p-4">
           <div className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white p-8 rounded-lg shadow-lg">
             <h1 className="text-2xl md:text-4xl font-bold mb-4">About Smart Retrieval</h1>
