3b95b1e121414cd66b3353ae2a09208ccaeb4717
Yee Kit <yeek3063@gmail.com>
2024-01-18 13:27:24 +0800

    Add Query Function Page

---
+++ frontend/app/query/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Header from "@/app/components/header";
+import Main from "@/app/components/ui/main-container";
+import QuerySection from "@/app/components/query-section";
+
+export default function Query() {
+
+  return (
+    <Main>
+      <Header />
+      <QuerySection />
+    </Main>
+  );
+}
