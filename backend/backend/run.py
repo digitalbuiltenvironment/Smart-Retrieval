000cb41d90ffa20d405061844d8896fcb06f7ab8
Yee Kit <yeek3063@gmail.com>
2024-02-29 16:36:56 +0800

    Update run.py to use environment settings

--- backend/backend/run.py
+++ backend/backend/run.py
@@ -1,4 +1,15 @@
+import os
+
 import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
+    environment = os.getenv("ENVIRONMENT", "dev")  # Default to 'development' if not set
+    if environment == "dev":
+        # Run the app with the development settings to auto reload
+        uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
+    if environment == "prod":
+        # Run the app with the production settings, no auto reload
+        uvicorn.run(app="main:app", host="0.0.0.0", reload=False)
