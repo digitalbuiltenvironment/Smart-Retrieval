3006c01fffa709cc9a22e63a18cc38139039284b
Yee Kit <yeek3063@gmail.com>
2024-03-18 15:38:44 +0800

    Updated auth with supabase backend & working SGID info retrieval & session checking

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,12 +1,59 @@
 import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next"
-import { getServerSession } from "next-auth"
+import type { GetServerSidePropsContext, NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import type { Adapter } from 'next-auth/adapters';
+import jwt from "jsonwebtoken";
+import { JWE, JWK } from 'node-jose';
+
+const makeUserinfoRequest = async (context: any) => {
+    console.log('context:', context);
+    const response = await fetch("https://api.id.gov.sg/v2/oauth/userinfo", {
+        headers: {
+            Authorization: `Bearer ${context.tokens.access_token}`,
+        },
+    })
+    const profile = await response.json()
+
+    // Decrypt the encrypted profile data
+    const privateKey = process.env.SGID_PRIVATE_KEY as string;
+    const decryptedProfile = await decryptData(profile.key, profile.data, privateKey);
+
+    // Build profile object
+    let newProfile = {
+        sub: profile.sub,
+        name: decryptedProfile['myinfo.name'],
+    }
+
+    // Return the decrypted profile
+    return newProfile
+}
+
+const decryptData = async (encKey: string, block: { [s: string]: unknown; } | ArrayLike<unknown>, privateKeyPem: string | object | Buffer | JWK.RawKey) => {
+    const result: { [key: string]: string } = {};
+
+    // Decrypted encKey to get block key
+    const privateKey = await JWK.asKey(privateKeyPem, 'pem');
+    const key = await JWE.createDecrypt(privateKey).decrypt(encKey);
+
+    // Parse the block key
+    const decryptedKey = await JWK.asKey(key.plaintext, 'json');
+
+    // Decrypt data
+    for (const [key, value] of Object.entries(block)) {
+        const { plaintext } = await JWE.createDecrypt(decryptedKey).decrypt(value as string);
+        result[key] = plaintext.toString('ascii');
+    }
+
+    return result
+}
 
 // You'll need to import and pass this
 // to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
 export const config = {
     // Configure one or more authentication providers
+    debug: true,    // Enable debug messages in the console if you are having problems
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -23,37 +70,79 @@ export const config = {
             },
             jwks_endpoint: 'https://api.id.gov.sg/v2/.well-known/jwks.json',
             token: 'https://api.id.gov.sg/v2/oauth/token',
-            userinfo: 'https://api.id.gov.sg/v2/oauth/userinfo',
+            userinfo: {
+                url: 'https://api.id.gov.sg/v2/oauth/userinfo',
+                // The result of this method will be the input to the `profile` callback.
+                async request(context) {
+                    // context contains useful properties to help you make the request.
+                    return await makeUserinfoRequest(context)
+                }
+            },
             checks: ['pkce', 'state'],
             idToken: true,
             client: { token_endpoint_auth_method: 'client_secret_post' },
             clientId: process.env.SGID_CLIENT_ID,
             clientSecret: process.env.SGID_CLIENT_SECRET,
-            profile(profile) {
+            profile: async (profile) => {
                 return {
                     id: profile.sub,
+                    name: profile.name,
                 }
             },
         },
     ],
+    // Persist accounts and session state to Supabase
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+    }) as Adapter,
     pages: {
         signIn: '/sign-in',
     },
+    session: {
+        // Choose how you want to save the user session.
+        // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+        // If you use an `adapter` however, we default it to `"database"` instead.
+        // You can still force a JWT session by explicitly defining `"jwt"`.
+        // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+        // which is used to look up the session in the database.
+        // strategy: "jwt",
+
+        // Seconds - How long until an idle session expires and is no longer valid.
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+
+        // Seconds - Throttle how frequently to write to database to extend a session.
+        // Use it to limit write operations. Set to 0 to always update the database.
+        // Note: This option is ignored if using JSON Web Tokens
+        updateAge: 24 * 60 * 60, // 24 hours
+    },
     callbacks: {
         async jwt({ token, user, account, profile }) {
-            // Persist the OAuth access_token to the token right after signin
+            // Persist the OAuth access_token and or the user id to the token right after signin
             if (account) {
-                token.id_token = account.id_token
-                token.provider = account.provider
                 token.accessToken = account.access_token
+                token.id = profile?.sub
             }
             return token
         },
         async session({ session, token, user }) {
             // Send properties to the client, like an access_token from a provider.
-            session.accessToken = token.accessToken as string
+            const signingSecret = process.env.SUPABASE_JWT_SECRET
+            if (signingSecret) {
+                const payload = {
+                    aud: "authenticated",
+                    exp: Math.floor(new Date(session.expires).getTime() / 1000),
+                    sub: user.id,
+                    email: user.email,
+                    role: "authenticated",
+                }
+                session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+                // session.jwt = token.jwt as string;
+                // session.id = token.id as string;
+            }
             return session
-        }
+        },
+
     }
 } satisfies NextAuthOptions
 
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,10 +1,73 @@
-export { default } from "next-auth/middleware"
+// export { default } from "next-auth/middleware"
 
 // export const config = {
-//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+//     matcher: ["/chat", "/search", "/query"] 
 // }
 
-// Ensure auth is required for all except the following paths
+// // Ensure auth is required for all except the following paths
+// export const config = {
+//     matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|favicon-16x16.png|apple-touch-icon.png|about|sign-in|api/status|privacy-policy|terms-of-service|sitemap.xml|robots.txt).+)']
+// };
+
+import { User } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js"
+
+export const middleware = async (request: NextRequest) => {
+    const { pathname, origin } = request.nextUrl;
+    const signinPage = new URL('/sign-in', origin);
+    // add params to the signinPage URL
+    signinPage.searchParams.set('callbackUrl', pathname);
+    const session = request.cookies.get('next-auth.session-token');
+    let sessionExpired = false;
+
+    if (!session) {
+        return NextResponse.redirect(signinPage.href, { status: 302 });
+    }
+    // console.log('session:', session);
+
+    // Check the database for the session token
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+        process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+        { db: { schema: 'next_auth' } },
+    );
+
+    const { data, error } = await supabase
+        .from('sessions')
+        .select('userId, expires')
+        .eq('sessionToken', session?.value)
+        .single();
+
+    if (!error) {
+        // console.log('data:', data);
+
+        // Check if the session is expired
+        const now = new Date().getTime();
+        const expires = new Date(data?.expires).getTime();
+        sessionExpired = expires > now ? true : false;
+
+        // Set a cookie to be used by the frontend to check if the session is expired
+        // request.cookies.set('next-auth.session-valid', true, {
+        //     httpOnly: true,
+        //     sameSite: 'lax',
+        //     secure: true,
+        //     path: '/',
+        //     maxAge: 60 * 60 * 24 * 1 // 1 day
+        // });
+    }
+    else {
+        console.error('Error fetching session from database:', error.message);
+    }
+
+    // Redirect to the sign-in page if the session is expired
+    if (pathname != "/sign-in" && !sessionExpired) {
+        return NextResponse.redirect(signinPage.href, { status: 302 });
+    }
+}
+
 export const config = {
-    matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|favicon-16x16.png|apple-touch-icon.png|about|sign-in|privacy-policy|terms-of-service|sitemap.xml|robots.txt).+)']
-};
+    matcher: ['/((?!api/auth|_next/static|_next/image|favicon.ico|favicon-16x16.png|apple-touch-icon.png|about|sign-in|api/status|privacy-policy|terms-of-service|sitemap.xml|robots.txt).+)']
+}
+
+type session = {} | User;
