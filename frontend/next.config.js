2b1d3dbb3d04ce4914f446556d063bcd89d302ae
Yee Kit <yeek3063@gmail.com>
2024-03-22 16:05:25 +0800

    Package Updates

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
   publicRuntimeConfig: {
     serverActions: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({ "bufferutil": "bufferutil", "utf-8-validate": "utf-8-validate", "supports-color": "supports-color" }); 
+    }
+    return config;
+  }
 };
 
 module.exports = nextConfig;
