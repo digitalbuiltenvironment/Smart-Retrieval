892cf0ce008c6768f6c8bc80c4afb49c3e029b82
Yee Kit <yeek3063@gmail.com>
2024-04-08 15:38:54 +0800

    Added supabase to healthcheck

--- backend/backend/app/api/routers/healthcheck.py
+++ backend/backend/app/api/routers/healthcheck.py
@@ -1,4 +1,7 @@
+import os
+
 from fastapi import APIRouter, Depends, Request
+from supabase import Client, ClientOptions, create_client
 
 from backend.app.utils import auth
 
@@ -23,12 +26,31 @@ async def healthcheck(
 
     # TODO: check if other services are ready
 
-    # logger.info("Healthcheck: {results}")
+    # Try to connect to supabase
+    supabase_url: str = os.environ.get("SUPABASE_URL")
+    supabase_key: str = os.environ.get("SUPABASE_ANON_KEY")
 
-    results = {"status": "OK"}
+    supabase: Client = create_client(
+        supabase_url=supabase_url,
+        supabase_key=supabase_key,
+        options=ClientOptions(
+            postgrest_client_timeout=10,
+            storage_client_timeout=10,
+        ),
+    )
+
+    response = supabase.table("users").select("id").execute()
+
+    if response.count >= 0:
+        results["supabase"] = True
+    else:
+        results["supabase"] = False
+
+    results["backend"] = True
+    # logger.info("Healthcheck: {results}")
     return results
 
 
 # Simple test to check if the healthcheck endpoint is working
 def test_healthcheck():
-    assert healthcheck() == {"status": "OK"}
+    assert healthcheck() == {"status": True, "supabase": True}
