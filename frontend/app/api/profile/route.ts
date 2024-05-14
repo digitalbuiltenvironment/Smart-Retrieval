506bbb91577fde6d456b09adc0a5f357844ffc26
Yee Kit <yeek3063@gmail.com>
2024-05-14 23:46:46 +0800

    Update route return error & removed redundant API Calls

--- frontend/app/api/profile/route.ts
+++ frontend/app/api/profile/route.ts
@@ -2,8 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const { pathname, origin } = request.nextUrl;
-    const signinPage = new URL('/sign-in', origin);
     // Retrieve the session token from the request cookies
     const session = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
 
@@ -25,7 +23,7 @@ export async function GET(request: NextRequest) {
 
     if (sessionError) {
         console.error('Error fetching session from database:', sessionError.message);
-        return NextResponse.redirect(signinPage.href, { status: 302 });
+        return NextResponse.json({ error: sessionError.message }, { status: 500 });
     }
 
     // Retrieve the user's profile data
@@ -37,7 +35,7 @@ export async function GET(request: NextRequest) {
 
     if (userError) {
         console.error('Error fetching user data from database:', userError.message);
-        return NextResponse.redirect(signinPage.href, { status: 302 });
+        return NextResponse.json({ error: userError.message }, { status: 500 });
     }
 
     // console.log('userData:', userData);
--- frontend/app/api/public-collections/route.ts
+++ frontend/app/api/public-collections/route.ts
@@ -2,37 +2,12 @@ import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    const { pathname, origin } = request.nextUrl;
-    const signinPage = new URL('/sign-in', origin);
-    // Retrieve the session token from the request cookies
-    const session = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token');
-
-    // Create a new Supabase client
-    const supabaseAuth = createClient(
-        process.env.SUPABASE_URL ?? '',
-        process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
-        { db: { schema: 'next_auth' } },
-    );
-
+   // Create a new Supabase client
     const supabase = createClient(
         process.env.SUPABASE_URL ?? '',
         process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
         { db: { schema: 'public' } },
     );
-
-    // Retrieve the user's ID from the session token
-    const { data: sessionData, error: sessionError } = await supabaseAuth
-        .from('sessions')
-        .select('userId')
-        .eq('sessionToken', session?.value)
-        .single();
-
-    // const userId = sessionData?.userId;
-
-    if (sessionError) {
-        console.error('Error fetching session from database:', sessionError.message);
-        return NextResponse.redirect(signinPage.href, { status: 302 });
-    }
 
     // Retrieve the public collections id and data from the database
     const { data: publicCollections, error: pubCollErr } = await supabase
@@ -41,7 +16,7 @@ export async function GET(request: NextRequest) {
 
     if (pubCollErr) {
         console.error('Error fetching public collection data from database:', pubCollErr.message);
-        return NextResponse.redirect(signinPage.href, { status: 302 });
+        return NextResponse.json({ error: pubCollErr.message }, { status: 500 });
     }
 
     // Extract collections object from each item in the array
